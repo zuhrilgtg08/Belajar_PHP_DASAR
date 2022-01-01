1da2f0340e7df9d107b10b42e6eb19ad08cd2742
zuhril08 <zuhrilfahrizal08gtg@gmail.com>
2022-01-01 18:28:49 +0700

    commit 01/01/2022

--- latihan14/login.php
+++ latihan14/login.php
@@ -31,32 +31,45 @@ if (isset($_POST["login"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
+
+    <!-- bootsrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h2>Halaman Login</h2>
 
-    <?php if(isset($error)) : ?>
-        <h5 style="color: red; font-style:italic;">username atau password salah</h5>
-    <?php endif; ?>
-    
-    <form action="" method="POST">
-        <ul>
-            <li>
-                <label for="username">username : </label>
-                <input type="text" name="username" id="username">
-            </li>
 
-            <li>
-                <label for="password">password : </label>
-                <input type="password" name="password" id="password">
-            </li>
+    <div class="card m-5">
+        <div class="card-header bg-warning">
+            <h2 class="text-center text-capitalize text-white">login akun</h2>
+        </div>
 
-            <li>
-                <button type="submit" name="login">Login</button>
-            </li>
-        </ul>
-    </form>
+        <?php if (isset($error)) : ?>
+            <div class="alert alert-danger text-capitalize" role="alert">
+                Maaf username atau password Salah.
+            </div>
+        <?php endif; ?>
+
+        <div class="card-body">
+            <form action="" method="POST" class="form-group mb-3">
+                <div class="mb-3">
+                    <label for="username" class="form-label text-capitalize">username : </label>
+                    <input type="text" name="username" id="username" class="form-control mb-3">
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label text-capitalize">password : </label>
+                    <input type="password" name="password" id="password" class="form-control mb-3">
+                </div>
+        </div>
+
+        <div class="card-footer">
+            <button type="submit" name="login" class="btn btn-primary" style="width: 100px;">Login</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
