3f6961feb2e025ae6b22d22057b9f5292f9ad438
zuhril08 <zuhrilfahrizal08gtg@gmail.com>
2022-01-01 16:40:15 +0700

    commit 01/01/2022

--- latihan13/registrasi.php
+++ latihan13/registrasi.php
@@ -1,19 +1,18 @@
 <?php
 require 'function.php';
 
-    if(isset($_POST["register"])){
+if (isset($_POST["register"])) {
 
-        if(registrasi($_POST) > 0){
-            echo "
+    if (registrasi($_POST) > 0) {
+        echo "
                 <script>
                     alert('user baru berhasil ditambahkan');
                 </script>
             ";
-        } else{
-            echo mysqli_error($conn);
-        }
-
+    } else {
+        echo mysqli_error($conn);
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -25,38 +24,39 @@ require 'function.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Registrasi</title>
 
-    <style>
-        label{
-            display: block;
-        }
-    </style>
+    <!-- boostrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Halaman Registrasi</h1>
+    <div class="card m-3 p-3">
+        <div class="card-header bg-success text-white text-center">
+            <h2>Registrasi Akun</h2>
+        </div>
 
-    <form action="" method="POST">
-        <ul>
-            <li>
-                <label for="username">username : </label>
-                <input type="text" name="username" id="username">
-            </li>
+        <div class="card-body">
+            <form action="" method="POST" class="form-group-sm">
+                <div class="mb-3">
+                    <label for="username" class="form-label text-capitalize">username : </label>
+                    <input type="text" name="username" id="username" class="form-control mb-3">
+                </div>
 
-            <li>
-                <label for="password">password : </label>
-                <input type="password" name="password" id="password">
-            </li>
+                <div class="mb-3">
+                    <label for="password" class="form-label text-capitalize">password : </label>
+                    <input type="password" name="password" id="password" class="form-control mb-3">
+                </div>
 
-            <li>
-                <label for="password2">konfirmasi password : </label>
-                <input type="password" name="password2" id="password2">
-            </li>
+                <div class="mb-3">
+                    <label for="password2" class="form-label text-capitalize">konfirmasi password : </label>
+                    <input type="password" name="password2" id="password2" class="form-control mb-3">
+                </div>
+        </div>
 
-            <li>
-                <button type="submit" name="register">Register</button>
-            </li>
-        </ul>
-    </form>
+        <div class="card-footer">
+                <button type="submit" name="register" class="btn btn-success">Register</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
