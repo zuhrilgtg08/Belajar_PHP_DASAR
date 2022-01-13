61bb66e8f79b9780e969e0a9826a7d7b4efed3d5
zuhril08 <zuhrilfahrizal08gtg@gmail.com>
2022-01-13 10:43:33 +0700

    perubahan extensions

---
+++ latihan19 (Tanpa Pagination)/function.php
@@ -0,0 +1,188 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// tampilkan data
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+
+    // siapkan wadah
+    $rows = [];
+    while ($baris = mysqli_fetch_assoc($result)) {
+        $rows[] = $baris;
+    }
+
+    return $rows;
+}
+
+
+
+
+// tambah data
+function tambah($data)
+{
+    // ambil data dari tiap elemen dalam form
+    global $conn;
+    $nama = htmlspecialchars($data["nama"]);
+    $nis = htmlspecialchars($data["nis"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    // upload gambar jalankan function upload gambar
+    $gambar = upload();
+
+    if (!$gambar) {
+        return false;
+    }
+
+    $query = "INSERT INTO mahasiswa
+                        VALUES
+                        ('', '$nama', '$nis', '$email', '$jurusan', '$gambar')";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// upload gambar
+function upload()
+{
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $erorr = $_FILES['gambar']['erorr'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    // cek jika tidak ada gambar yang di upload
+    if ($erorr === 4) {
+        echo "<script>
+                    alert('pilih gambar terlebih dahulu');
+                </script>";
+        return false;
+    }
+
+
+    // cek apakah yang diupload adalah gambar
+    $extensiGambarvalid = ['jpg', 'jpeg', 'png'];
+    $extensiGambar = explode('.', $namaFile);
+    $extensiGambar = strtolower(end($extensiGambar));
+
+    if (!in_array($extensiGambar, $extensiGambarvalid)) {
+        echo "<script>
+                    alert('Maaf yang anda upload bukan gambar !');
+                </script>";
+        return false;
+    }
+
+    // cek jika ukurannya terlalu besar
+    if ($ukuranFile > 5000000) {
+        echo "<script>
+                    alert('Maaf file yang anda upload ukurannya terlalu besar !');
+                </script>";
+        return false;
+    }
+
+
+    // geberate nama gambar baru
+    $namaFilebaru = uniqid();
+    $namaFilebaru .= '.';
+    $namaFilebaru .= $extensiGambar;
+
+    // berhasil pengecekan gambar siap diupload
+    move_uploaded_file($tmpName, 'images/' . $namaFilebaru);
+
+    return $namaFilebaru;
+}
+
+// hapus data
+function hapus($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+// ubah data
+function ubah($data)
+{
+    global $conn;
+
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nis = htmlspecialchars($data["nis"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+
+    // cek user apakah user upload gambar baru atau tidak
+    if ($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    } else {
+        $gambar = upload();
+    }
+
+    $query = "UPDATE mahasiswa SET 
+                nama = '$nama',
+                nis = '$nis',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+            WHERE id = $id
+            ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+    $query = "SELECT * FROM mahasiswa WHERE
+                nama LIKE '%$keyword%' OR
+                nis LIKE '%$keyword%' OR
+                email LIKE '%$keyword%' OR
+                jurusan LIKE '%$keyword%'
+            ";
+    return query($query);
+}
+
+function registrasi($data)
+{
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    // cek username udah ada atau belum
+    $result = mysqli_query($conn, "SELECT username FROM users WHERE username = '$username'");
+
+    if(mysqli_fetch_assoc($result)){
+        echo "
+                <script>
+                    alert('username sudah terdaftar');
+                </script>
+            ";
+        return false;
+    }
+
+    // cek konfirmasi password
+    if ($password !== $password2) {
+        echo "
+            <script>
+                alert('konfirmasi password tidak sesuai');
+            </script>
+        ";
+        return false;
+    }
+    // enkripsi passwordnya
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+
+    // tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO users VALUES('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
---
+++ latihan19 (Tanpa Pagination)/index.php
@@ -0,0 +1,168 @@
+<?php
+session_start();
+// if (!isset($_SESSION["login"])) {
+//     header("Location: login.php");
+//     exit;
+// }
+//mengkoneksikan dengan database
+require 'function.php';
+// accending dan descending
+// ASC DAN DESC adalah sebuah urutan mulai dari kecil ke besar begitu juga sebaliknya
+//ambil data dari database
+
+// config pagination
+$jumlahDataPerhalaman = 10;
+// $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+// $jumlahData = mysqli_num_rows($result);
+$jumlahData = count(query("SELECT * FROM mahasiswa"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
+$pageAktif = (isset($_GET["page"])) ? $_GET["page"] : 1;
+$awalData = ($jumlahDataPerhalaman * $pageAktif) - $jumlahDataPerhalaman;
+
+// if(isset($_GET["page"])){
+//     $pageAktif = $_GET["page"];
+// }else{
+//     $pageAktif = 1;
+// }
+
+
+// pageAktif = 5; awalData = 0
+// pageAktif = 5; awalData = 5
+
+
+
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerhalaman");
+// fungsi round() membulatkan bilangan pecahan ke bilangan desimal terdekat
+// sedangkan floor() membulatkan bilangan pecahan ke bawah
+// lalu ceil() membualtkan bialangan ke atas
+// jika tombol cari ditekan
+if (isset($_POST["cari"])) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+
+    <!-- boostrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- style css custom -->
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/main.css" rel="stylesheet" />
+
+    <style>
+        .card {
+            width: 10rem;
+            /* float: right; */
+            text-decoration: none;
+        }
+
+        .card a:hover{
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h4>Dasboard</h4>
+        </div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+
+        <div class="menu-toggle">
+            <input type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+
+    <a href="logout.php" class="btn btn-danger float-end m-3">Logout</a>
+
+    <h1 class="text-center mt-2 text-uppercase">Daftar Mahasiswa</h1>
+
+    <a href="tambah.php" class="btn btn-warning m-4">Tambah Data Mahasiswa</a>
+
+    <!-- cari form data -->
+    <form action="" method="POST" class="form-group m-3">
+        <input type="search" name="keyword" class="form-control" autofocus placeholder="Cari Mahasiswa..." autocomplete="off">
+        <button type="submit" name="cari" class="bg-primary rounded mt-3" style="width: 100px; color:#fff;">Cari Data</button>
+    </form>
+
+
+
+
+    <div class="table-responsive mt-3">
+        <table class="table table-striped text-center">
+            <tr class="table table-dark">
+                <th>No.</th>
+                <th>Aksi</th>
+                <th>Gambar</th>
+                <th>NIS</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+            </tr>
+
+            <?php $i = 1; ?>
+            <?php foreach ($mahasiswa as $mhs) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td>
+                        <button class="bg-success"><a href="ubah.php?id=<?= $mhs["id"]; ?>">Ubah</a> </button>
+                        <button class="bg-danger"><a href="hapus.php?id=<?= $mhs["id"]; ?>" onclick="return confirm('Yakin ingin dihapus');">Hapus</a></button>
+                    </td>
+                    <td><img src="images/<?= $mhs["gambar"]; ?>" width="50" alt=""></td>
+                    <td><?= $mhs["nis"]; ?></td>
+                    <td><?= $mhs["nama"]; ?></td>
+                    <td><?= $mhs["email"]; ?></td>
+                    <td><?= $mhs["jurusan"]; ?></td>
+                </tr>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        </table>
+    </div>
+
+    <!-- tombol pagination -->
+    <div class="card bg-primary m-3">
+        <div class="card-body">
+            <?php if ($pageAktif > 1) : ?>
+                <a href="?page=<?= $pageAktif - 1; ?>" class="fs-5">&laquo;</a>
+            <?php endif; ?>
+
+            <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+                <?php if ($i == $pageAktif) : ?>
+                    <a href="?page=<?= $i; ?>" class="btn btn-warning text-danger"><?= $i; ?></a>
+                <?php else : ?>
+                    <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+                <?php endif; ?>
+            <?php endfor; ?>
+
+            <?php if ($pageAktif < $jumlahHalaman) : ?>
+                <a href="?page=<?= $pageAktif + 1; ?>" class="fs-5">&raquo;</a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <!-- script.js -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
