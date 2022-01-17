7eb56f53f7a9217f5048b02954286fce9cc0c277
zuhril08 <zuhrilfahrizal08gtg@gmail.com>
2022-01-17 08:40:00 +0700

    17/01/2022

--- latihan23 PDF_REPORTING/cetak.php
+++ latihan23 PDF_REPORTING/cetak.php
@@ -1,18 +1,47 @@
 <?php
-	$html = '
-	<html> 
-	<body>
-		<h1>Hello World!</h1> 
-		<p>Ini paragraf 1</p>
-	</body>
-	</html>
-	';
+require 'function.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
 
-	$word = require 'LEMBAR VAKSINASI.pdf';
+	$html = '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Daftar Mahasiswa</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+
+            <table border="1" cellpadding="10" collspacing="0">
+                <tr>
+                    <th>No.</th>
+                    <th>Gambar</th>
+                    <th>NIS</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Jurusan</th>
+                </tr>';
+
+		foreach($mahasiswa as $mhs){
+			$html .= '<tr>
+				<td>'. $i++ .'</td>
+				<td><img src="images/'. $mhs["gambar"] .'" width="50"></td>
+				<td>'. $mhs["nis"] . '</td>
+				<td>' . $mhs["nama"] . '</td>
+				<td>' . $mhs["email"] . '</td>
+				<td>' . $mhs["jurusan"] . '</td>
+			</tr>';
+		}
+		
+$html .='</table>
+</body>
+</html>';
 
 	require_once "library PDF/mpdf_v8.0.3-master/vendor/autoload.php";
 	$mpdf = new \Mpdf\Mpdf();
-	$mpdf->AddPage("P","","","","","15","15","15","15","","","","","","","","","","","","A4");
-	$mpdf->WriteHTML($word);
-	$mpdf->Output();
+	// $mpdf->AddPage("P","","","","","15","15","15","15","","","","","","","","","","","","A4");
+	$mpdf->WriteHTML($html);
+	$mpdf->Output('Daftar Mahasiswa.pdf', 'I');
 ?>
+
